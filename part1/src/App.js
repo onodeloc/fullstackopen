21af927e5d828d6b2f53f4ada5caf43cd576fe33
Cole Beezley <cdbbeezley@gmail.com>
2023-03-05 12:41:52 -0800

    1.12*: anecdotes step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,6 +1,24 @@
 import { useState } from 'react'
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const randomizeAnecdote = () => {
+    var randNum = Math.floor(Math.random() * 8)
+    setSelected(randNum)
+  }
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -30,6 +48,12 @@ const App = () => {
       <Statistics
         good={good} neutral={neutral} bad={bad} all={all}
       />
+      <br />
+      <div>
+        {anecdotes[selected]}
+        <br />
+        <Button handleClick={randomizeAnecdote} text='next anecdote' />
+      </div>
     </div>
   )
 }

662c0b0f3b2a8c06c3bbf6150bf85a5c3b49878a
Cole Beezley <cdbbeezley@gmail.com>
2023-03-05 12:03:43 -0800

    1.7: unicafe step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,15 +4,19 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
@@ -21,13 +25,15 @@ const App = () => {
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      
+
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {((good) + (bad * -1)) / all}</p>
+      <p>positive {good / all}%</p>
     </div>
   )
 }
-
 export default App
